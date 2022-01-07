f005ed1a49be263b5e2681e43bd99d5691d38acf
John <john.brucker23@d155.org>
2022-01-07 07:59:40 -0600

    Initial Commit

---
+++ chickenApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chickenApp
+//
+//  Created by JOHN BRUCKER on 1/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
