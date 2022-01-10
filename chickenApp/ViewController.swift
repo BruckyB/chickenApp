b61caa47c922cd88cf77bdba5e70f87b968f9e6a
bryan <bryan.ruiz23@d155.org>
2022-01-10 08:34:55 -0600

    chicken Collection

--- chickenApp/ViewController.swift
+++ chickenApp/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var chickens:Chicken = [Chicken]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
