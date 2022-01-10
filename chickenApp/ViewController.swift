4c04ebae7f2e994ba67061e2b10a74de91f9e19a
John <john.brucker23@d155.org>
2022-01-10 08:33:10 -0600

    button

--- chickenApp/ViewController.swift
+++ chickenApp/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func fButton(_ sender: Any) {
+        performSegue(withIdentifier: "freezerSegue", sender: nil)
+    }
+    
 
 }
 
