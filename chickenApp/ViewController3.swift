0edf5598f5d0c46de82f69c6960208dde13a7c2f
John <john.brucker23@d155.org>
2022-01-11 08:04:43 -0600

    things

---
+++ chickenApp/ViewController3.swift
@@ -0,0 +1,121 @@
+//
+//  ViewController3.swift
+//  chickenApp
+//
+//  Created by JOHN BRUCKER on 1/11/22.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func segAction(_ sender: Any) {
+    }
+    
+    @IBAction func b1(_ sender: UIButton) {
+    }
+    @IBAction func b2(_ sender: UIButton) {
+    }
+    @IBAction func b3(_ sender: UIButton) {
+    }
+    @IBAction func b4(_ sender: UIButton) {
+    }
+    @IBAction func b5(_ sender: UIButton) {
+    }
+    @IBAction func b6(_ sender: UIButton) {
+    }
+    @IBAction func b7(_ sender: UIButton) {
+    }
+    @IBAction func b8(_ sender: UIButton) {
+    }
+    @IBAction func b9(_ sender: UIButton) {
+    }
+    @IBAction func b10(_ sender: UIButton) {
+    }
+    @IBAction func b11(_ sender: UIButton) {
+    }
+    @IBAction func b12(_ sender: UIButton) {
+    }
+    @IBAction func b13(_ sender: UIButton) {
+    }
+    @IBAction func b14(_ sender: UIButton) {
+    }
+    @IBAction func b15(_ sender: UIButton) {
+    }
+    @IBAction func b16(_ sender: UIButton) {
+    }
+    @IBAction func b17(_ sender: UIButton) {
+    }
+    @IBAction func b18(_ sender: UIButton) {
+    }
+    @IBAction func b19(_ sender: UIButton) {
+    }
+    @IBAction func b20(_ sender: UIButton) {
+    }
+    @IBAction func b21(_ sender: UIButton) {
+    }
+    @IBAction func b22(_ sender: UIButton) {
+    }
+    @IBAction func b23(_ sender: UIButton) {
+    }
+    @IBAction func b24(_ sender: UIButton) {
+    }
+    @IBAction func b25(_ sender: UIButton) {
+    }
+    @IBAction func b26(_ sender: UIButton) {
+    }
+    @IBAction func b27(_ sender: UIButton) {
+    }
+    @IBAction func b28(_ sender: UIButton) {
+    }
+    @IBAction func b29(_ sender: UIButton) {
+    }
+    @IBAction func b30(_ sender: UIButton) {
+    }
+    @IBAction func b31(_ sender: UIButton) {
+    }
+    @IBAction func b32(_ sender: UIButton) {
+    }
+    @IBAction func b33(_ sender: UIButton) {
+    }
+    @IBAction func b34(_ sender: UIButton) {
+    }
+    @IBAction func b35(_ sender: UIButton) {
+    }
+    @IBAction func b36(_ sender: UIButton) {
+    }
+    @IBAction func b37(_ sender: UIButton) {
+    }
+    @IBAction func b38(_ sender: UIButton) {
+    }
+    @IBAction func b39(_ sender: UIButton) {
+    }
+    @IBAction func b40(_ sender: UIButton) {
+    }
+    @IBAction func b41(_ sender: UIButton) {
+    }
+    @IBAction func b42(_ sender: UIButton) {
+    }
+    @IBAction func b43(_ sender: UIButton) {
+    }
+    @IBAction func b44(_ sender: UIButton) {
+    }
+    @IBAction func b45(_ sender: UIButton) {
+    }
+    @IBAction func b46(_ sender: UIButton) {
+    }
+    @IBAction func b47(_ sender: UIButton) {
+    }
+    @IBAction func b48(_ sender: UIButton) {
+    }
+    @IBAction func b49(_ sender: UIButton) {
+    }
+    @IBAction func b50(_ sender: UIButton) {
+    }
+}
