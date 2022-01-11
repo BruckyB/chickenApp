c5376630f9671a03cd7ac7d650900851fc3a9d8e
John <john.brucker23@d155.org>
2022-01-11 07:38:40 -0600

    ui

---
+++ chickenApp/ViewController2.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController2.swift
+//  chickenApp
+//
+//  Created by JOHN BRUCKER on 1/11/22.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
