fb4d73bf67485e8b221f8ccc5f6dce44cd2293e2
bryan <bryan.ruiz23@d155.org>
2022-02-23 07:34:49 -0600

    pushed

--- chickenApp/HelpScreen.swift
+++ chickenApp/HelpScreen.swift
@@ -23,7 +23,41 @@ class HelpScreen: UIViewController {
             if let encoded = try? encoder.encode(ViewController3.chicken) {
                     UserDefaults.standard.set(encoded, forKey: "SDATA")
             }
-
+            ChickenClass.notUsedR.removeAll()
+            ChickenClass.notUsedN.removeAll()
+            ChickenClass.notUsedSF.removeAll()
+            ChickenClass.notUsedST.removeAll()
+            ChickenClass.notUsedGF.removeAll()
+            ChickenClass.notUsedGN.removeAll()
+            
+            ChickenClass.freezerR.removeAll()
+            ChickenClass.freezerN.removeAll()
+            ChickenClass.freezerSF.removeAll()
+            ChickenClass.freezerST.removeAll()
+            ChickenClass.freezerGF.removeAll()
+            ChickenClass.freezerGN.removeAll()
+            
+            ChickenClass.thawR.removeAll()
+            ChickenClass.thawN.removeAll()
+            ChickenClass.thawSF.removeAll()
+            ChickenClass.thawST.removeAll()
+            ChickenClass.thawGF.removeAll()
+            ChickenClass.thawGN.removeAll()
+            
+            ChickenClass.breadingR.removeAll()
+            ChickenClass.breadingN.removeAll()
+            ChickenClass.breadingSF.removeAll()
+            ChickenClass.breadingST.removeAll()
+            ChickenClass.breadingGF.removeAll()
+            ChickenClass.breadingGN.removeAll()
+            
+            ChickenClass.notUsedR = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+            ChickenClass.notUsedN = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+            ChickenClass.notUsedSF = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+            ChickenClass.notUsedST = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+            ChickenClass.notUsedGF = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+            ChickenClass.notUsedGN =
+            [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
             print("blah")
             print("oh yea")
         }))
